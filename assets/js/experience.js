f05a0e7dceab268d2fbb84b4777ba14418d2f2d7
Mayanklna <65867054+Mayanklna@users.noreply.github.com>
2022-12-20 22:17:35 +0530

    Update experience.js

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -6,21 +6,21 @@ const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
     title: "Android App Development Intern",
-    cardImage: "sssoi.jpg",
+    
     place: "Society Of Artificial Intelligence (SOAI)",
     time: "(July, 2020 - Nov 2020)",
     desp: "<li>Developed the prototype of an app Finders, a social media‑like platform for entrepreneurs to connect with investors.</li> <li>Designed and Implemented the chat inbox UI, linked the server APIs with the app using Retrofit3 android library.</li> <li>Worked on establishing the connection of the app with the AWS S3 bucket to store short‑pitch videos.</li><li>Developed the navigation drawer and implemented registration/login functionalities for both founders and Investors.</li>",
   },
   {
     title: "Web Devlopment Internship",
-    cardImage: "assets/images/experience-page/gsoc.png",
+   
     place: "Indian Institute Of Technology Roorkee",
     time: "(May, 2022 - Jun, 2022)",
     desp: "<li>Worked under professor Alok Bhardwaj, IIT Roorkee, on the development of a web‑based app for the retrieval and visualization of satellite data.</li><li>Utilized HTML, CSS, JavaScript, and Node.Js for the smooth retrieval of flood event data based on date ranges.</li>",
   },
   {
     title: "Research Intern",
-    cardImage: "IITR.jpg",
+   
     place: "Indian Institute Of Technology Roorkee",
     time: "(Jan, 2022 - Mar, 2022)",
     desp: "<li>Worked under professor Alok Bhardwaj, IIT Roorkee, in extracting optical metadata and microwave metadata using web scraping.</li><li>Utilized the scraped data from planet and scihub website for the study of floods in India, Indonesia, Japan, Laos, and Thailand.</li>",
